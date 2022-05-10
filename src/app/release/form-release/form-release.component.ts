1a95dd7573471578208893891f9cf34b487fd99a
Eduardo Araya <eduardojezine@gmail.com>
2022-05-10 15:20:47 -0300

    Up application

---
+++ src/app/release/form-release/form-release.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-release',
+  templateUrl: './form-release.component.html',
+  styleUrls: ['./form-release.component.scss']
+})
+export class FormReleaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/release/release.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormReleaseComponent } from './form-release/form-release.component';
+
+@NgModule({
+  declarations: [FormReleaseComponent],
+  imports: [CommonModule],
+})
+export class ReleaseModule {}
